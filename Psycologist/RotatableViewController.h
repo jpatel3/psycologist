8793ff0de0bf0ef8a30fa39f599a71134ca56028
jaimin patel <jaimin9999@gmail.com>
2013-03-12 00:51:43 -0400

    added support for rotation.

---
+++ Psycologist/RotatableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RotatableViewController.h
+//  Psycologist
+//
+//  Created by Jaimin Patel on 3/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotatableViewController : UIViewController
+
+@end
---
+++ Psycologist/RotatableViewController.m
@@ -0,0 +1,18 @@
+//
+//  RotatableViewController.m
+//  Psycologist
+//
+//  Created by Jaimin Patel on 3/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "RotatableViewController.h"
+
+@implementation RotatableViewController
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
