a846c682220b9094d4d512d9ca267caa8b6f9e1e
jaimin patel <jaimin9999@gmail.com>
2013-03-12 12:18:08 -0400

    enabled for iPad and added toolbar

---
+++ Psycologist/SplitViewBarButtonItemPresenter.h
@@ -0,0 +1,15 @@
+//
+//  SplitViewBarButtonItemPresenter.h
+//  Psycologist
+//
+//  Created by Jaimin Patel on 3/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SplitViewBarButtonItemPresenter <NSObject>
+
+@property(nonatomic, strong)UIBarButtonItem *splitViewBarButtonItem;
+
+@end
